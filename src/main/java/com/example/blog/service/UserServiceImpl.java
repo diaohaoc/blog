9dcfc47435b5f50549b47be859ad9cbcf82f248a
Diao-hao <522339634@qq.com>
2020-03-30 21:02:11 +0800

    个人博客

---
+++ src/main/java/com/example/blog/service/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.blog.service;
+
+import com.example.blog.dao.UserRespository;
+import com.example.blog.po.User;
+import com.example.blog.util.MD5Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRespository userRespository;
+
+    @Override
+    public User checkUser(String username, String password) {
+        User user = userRespository.findByUsernameAndPassword(username, MD5Utils.code(password));
+        return user;
+    }
+}
